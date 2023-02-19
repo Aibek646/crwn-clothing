9d83a41fcd766d6421058d9f2f44d43a956ca838
Aibek Ramazanov <aibekramazanov@Aibeks-MBP.lan>
2023-02-19 08:37:50 -0600

    sign-up form component with button styles

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,102 @@
+import {
+    createAuthUserWithEmailAndPassword,
+    createUserDocumentFromAuth
+} from "../../routes/utils/firebase/firesbase.utils";
+
+import "./sign-up-form.styles.scss";
+import { useState } from "react";
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+
+const defaultFormFields = {
+    displayName: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+};
+
+const SignUpForm = () => {
+    const [formFields, setFormFields] = useState(defaultFormFields);
+    const { displayName, email, password, confirmPassword } = formFields;
+
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (password !== confirmPassword) {
+            alert("password do not match");
+            return;
+        }
+
+        try {
+            const { user } = await createAuthUserWithEmailAndPassword(
+                email,
+                password
+            );
+            await createUserDocumentFromAuth(user, { displayName });
+            resetFormFields();
+        } catch (error) {
+            if (error.code === "auth/email-already-in-use") {
+                alert("Cannot create user, email already in use");
+            } else {
+                console.log("user creation encountered an error");
+            }
+        }
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormFields({ ...formFields, [name]: value });
+    };
+
+    return (
+        <div className="sign-up-container">
+            <h2>Don't have an account?</h2>
+            <h1>Sign up with your email and password</h1>
+            <form onSubmit={handleSubmit}>
+                <FormInput
+                    label="Display Name"
+                    type="text"
+                    required
+                    name="displayName"
+                    value={displayName}
+                    onChange={handleChange}
+                />
+
+                <FormInput
+                    label="Email"
+                    type="email"
+                    required
+                    name="email"
+                    value={email}
+                    onChange={handleChange}
+                />
+
+                <FormInput
+                    label="Password"
+                    type="password"
+                    required
+                    name="password"
+                    value={password}
+                    onChange={handleChange}
+                />
+
+                <FormInput
+                    label="Confirm Password"
+                    type="password"
+                    required
+                    name="confirmPassword"
+                    value={confirmPassword}
+                    onChange={handleChange}
+                />
+                <Button buttonType="google" type="submit">
+                    Sign up
+                </Button>
+            </form>
+        </div>
+    );
+};
+
+export default SignUpForm;
--- src/routes/utils/firebase/firesbase.utils.js
+++ src/routes/utils/firebase/firesbase.utils.js
@@ -1,1 +1,73 @@
 import { initializeApp } from "firebase/app";
+import {
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword
+} from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyArA1pVSJtn9BV3Tv57GdOqJsaa9GebdlU",
+    authDomain: "crwn-clothing-db-8ec55.firebaseapp.com",
+    projectId: "crwn-clothing-db-8ec55",
+    storageBucket: "crwn-clothing-db-8ec55.appspot.com",
+    messagingSenderId: "360357114751",
+    appId: "1:360357114751:web:a11bdf3429c48d153ba04a",
+    measurementId: "G-MD0Y6LPPWM"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+
+export const signInWithGooglePopup = () =>
+    signInWithPopup(auth, googleProvider);
+
+export const signInWithGoogleRedirect = () =>
+    signInWithRedirect(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (
+    userAuth,
+    additionalInformation = {}
+) => {
+    const userDocRef = doc(db, "users", userAuth.uid);
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt,
+                ...additionalInformation
+            });
+        } catch (error) {
+            console.log("error creating the user", error.message);
+        }
+    }
+
+    return userDocRef;
+
+    //if user data exists
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+    return await createUserWithEmailAndPassword(auth, email, password);
+};
